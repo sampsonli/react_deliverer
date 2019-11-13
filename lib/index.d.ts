395961538d35b4c18bfc119aa4675b63c1b10975
lichun <740056710@qq.com>
2019-11-13 09:44:26 +0800

    :+1:修复bug

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export declare function connect<N>(ns: N): Function;
+export declare function action(clazz: any, act: any): any;
+declare const _default: (store: any, asyncReducers?: {}) => void;
+export default _default;
