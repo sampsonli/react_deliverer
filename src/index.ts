9e622e1c2c03f4a177980e30b4e9fb2a8a6debde
lichun <740056710@qq.com>
2019-11-29 11:29:35 +0800

    优化

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,72 @@
 import {combineReducers} from 'redux';
 
-let _store = undefined;
-let _asyncReducers = undefined;
-
 declare const module;
+let _store = undefined;
+let _asyncReducers = {};
 
-export function connect<N>(ns: N): Function {
+function injectReducer(key, reducer) {
     if (_store) {
-        const injectReducer = (key, reducer) => {
-            _asyncReducers[key] = reducer;
-            _store.replaceReducer(combineReducers({
-                ..._asyncReducers,
-            }));
-        };
-        if (_asyncReducers[ns] && !module.hot) {
+        if (_asyncReducers[key] && !module.hot) {
             console.error('模块命名重复，可能会引发未知错误');
         }
-        return function (clazz) { // 注入 clazz 代表目标类
-            return function () {  // 构造函数, 返回新的类
-                const result = new clazz();
-                result.ns = ns;
-                const actions = clazz.prototype.__actions || {};
-                const mutations = {};
-                Object.keys(actions).forEach(func => {
-                    mutations[`${ns}/${func}`] = actions[func]
-                });
-                const reducer = (state = JSON.parse(JSON.stringify(result)), {type, payload}) => {
-                    if (mutations[type]) {
-                        const curr = {...state};
-                        state = mutations[type].bind(curr)(payload) || curr;
-                        for(let key in state) {
+        _asyncReducers[key] = reducer;
+        _store.replaceReducer(combineReducers({
+            ..._asyncReducers,
+        }));
+    } else {
+        throw new Error('deliverer 未初始化, 请先调用 deliverer(store)')
+    }
+}
+
+
+export function connect<N>(ns: N): Function {
+    return function (clazz) { // 注入 clazz 代表目标类
+        return function () {  // 构造函数, 返回新的类
+            const result = new clazz();
+            result.ns = ns;
+            const actions = clazz.prototype.__actions || {};
+            const mutations = {};
+            Object.keys(actions).forEach(func => {
+                mutations[`${ns}/${func}`] = actions[func]
+            });
+            const reducer = (state = JSON.parse(JSON.stringify(result)), {type, payload}) => {
+                if (mutations[type]) {
+                    const curr = {...state};
+                    const modify = mutations[type].bind(curr)(payload, curr) || curr;
+                    const diff = Object.keys(modify).some(key => modify[key] !== state[key]);
+                    if(diff) {
+                        state = modify;
+                        for (let key in state) {
                             result[key] = state[key];
                         }
                     }
-                    return state;
-                };
-                injectReducer(ns, reducer);
-                const rootState = _store.getState();
-                const state = rootState[ns];
-                if(state) { // 老数据同步进去
-                    for(let key in state) {
-                        result[key] = state[key];
-                    }
                 }
-                return result;
-
-
+                return state;
+            };
+            injectReducer(ns, reducer);
+            const rootState = _store.getState();
+            const state = rootState[ns];
+            if (state) { // 老数据同步进去
+                for (let key in state) {
+                    result[key] = state[key];
+                }
             }
-
+            return result;
         }
-    } else {
-        throw new Error('deliverer 未初始化, 请先调用 deliverer(store)')
+
     }
 
 
 }
 
 export function action(clazz, act) {
-    if(clazz.__actions) {
+    if (clazz.__actions) {
         clazz.__actions[act] = clazz[act]
     } else {
         clazz.__actions = {[act]: clazz[act]}
     }
     clazz[act] = function (payload) {
-        _store.dispatch({ type: this.ns + "/" + act, payload: payload });
+        _store.dispatch({type: this.ns + "/" + act, payload: payload});
     };
     return clazz[act]
 }
