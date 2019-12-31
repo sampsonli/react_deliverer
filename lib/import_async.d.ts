cd7240fb72f0f1c42c3087ee0c872f557e95da93
lichun <lichun@mistong.com>
2019-12-31 15:01:24 +0800

    优化

---
+++ lib/import_async.d.ts
@@ -0,0 +1,7 @@
+declare const _default: (_ref: any) => {
+    manipulateOptions: (opts: any, parserOpts: any) => void;
+    visitor: {
+        Import: (path: any) => void;
+    };
+};
+export default _default;
