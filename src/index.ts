d56ce79886406e0cca04bb936c4fe48fb41c7c5c
lichun <lichun@mistong.com>
2020-04-26 09:37:01 +0800

    adddoc

--- src/index.ts
+++ src/index.ts
@@ -115,6 +115,11 @@ export function deliver(namespace: string|Function): Function {
                     }
                 }
             });
+
+            /**
+             * 设置模块数据
+             * @param props， 要设置属性的集合， 为普通对象，比如 {a: 1, b:2}, 代表设置模块中a属性为1， b属性为2
+             */
             prototype.setData = function(props) {
                 const state = _store.getState()[ns];
                 const keys = Object.keys(props)
@@ -130,6 +135,10 @@ export function deliver(namespace: string|Function): Function {
                 }
 
             }
+            /**
+             * react hooks写法获取模块中数据
+             * @param prop 可以为 字符串：模块中某个属性，方法：模块属性选择器， 空：整个模块数据
+             */
             prototype.useData = function (prop = null) {
                 if (useState && useEffect) {
                     const [data, setData] = useState('__UNINITED__');
@@ -167,6 +176,13 @@ export function deliver(namespace: string|Function): Function {
                 }
             };
 
+            /**
+             * 重置模块数据到初始状态， 一般用于组件销毁的时候调用
+             */
+            prototype.reset = function () {
+                _store.dispatch({type: `deliver/${ns}`, payload: initState});
+            };
+
 
             const initState = {};
             Object.getOwnPropertyNames(instance).forEach(key => {
@@ -175,9 +191,6 @@ export function deliver(namespace: string|Function): Function {
                 mapReverse[key] = prop;
                 initState[prop] = instance[key];
             });
-            prototype.reset = function () {
-                _store.dispatch({type: `deliver/${ns}`, payload: initState});
-            };
             const reducer = (state = initState, {type, payload}) => {
                 if (type === `deliver/${ns}`) {
                     return payload;
