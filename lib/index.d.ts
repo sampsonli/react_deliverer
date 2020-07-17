78ccff8db7fc472873a5caad1adfc5b31f76f8a3
lichun <lichun@mistong.com>
2020-07-17 14:10:23 +0800

    优化

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,8 @@
 export declare function deliver(namespace: string | Function): Function;
+/**
+ * 通过hook方式获取模块中数据
+ * @param ns 模块名称
+ */
 export declare const useData: (ns: string) => any;
 declare const _default: (store: any, asyncReducers?: {}) => void;
 export default _default;
